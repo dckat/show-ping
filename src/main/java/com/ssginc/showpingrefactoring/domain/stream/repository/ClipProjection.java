b6c2d6fbaecf96ff18ea4ef9423c058470c1f523
Kim Daecheol <kdc9619@naver.com>
2026-04-06 17:07:36 +0900

    Feat: 네이티브 쿼리 프로젝션 인터페이스 구현

---
+++ src/main/java/com/ssginc/showpingrefactoring/domain/stream/repository/ClipProjection.java
@@ -0,0 +1,9 @@
+package com.ssginc.showpingrefactoring.domain.stream.repository;
+
+public interface ClipProjection {
+
+    Long getClipNo();
+    String getStreamTitle();
+    String getClipPath();
+
+}
